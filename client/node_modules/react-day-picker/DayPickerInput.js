e04ebe6f24101c926c041cb5d5ca1d028cb0561f
Star Lee <zabra15@yahoo.com>
2018-01-13 13:05:21 -0500

    UserSettingsPage.js and userSettings.css added.

---
+++ client/node_modules/react-day-picker/DayPickerInput.js
@@ -0,0 +1,10 @@
+/*
+  Used to import DayPickerInput. e.g. `import DayPickerInput from 'react-day-picker/DayPickerInput'`
+*/
+
+/* eslint-disable no-var */
+/* eslint-env node */
+
+var DayPickerInput = require('./lib/src/DayPickerInput');
+
+module.exports = DayPickerInput;
