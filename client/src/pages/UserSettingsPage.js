8b0a80621af6a17eb16a938ed1aaef81990f11b3
Star Lee <zabra15@yahoo.com>
2018-01-13 13:05:21 -0500

    UserSettingsPage.js and userSettings.css added.

--- client/src/pages/UserSettingsPage.js
+++ client/src/pages/UserSettingsPage.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { Row, Input } from 'react-materialize';
+import '../styles/userSettings.css';
+import 'react-day-picker/lib/style.css';
 
+
+import DatePicker from '../components/DatePicker/DatePicker';
 
 class UserSettings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        //send to db
+            //calculate days until due date
+            //depending on how many days, add xxx calories to their calorie entry
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
-                <h1>UserSettings Page</h1>
+                <Row>
+                    <div className='col s8 offset-s2'>
+                    <h4>Settings</h4>
+                        <p class="question-heading">Typical Calorie Intake (before pregnancy):</p>
+                    <Input placeholder="Calories" s={6} name="calories" />
+                    </div>
+                </Row>
+                <Row>
+                    <div className='col s8 offset-s2'>
+                    <p class="question-heading">Due Date:</p>
+                    <DatePicker/>
+                    <Input type="submit" value="Submit" />
+                    </div>
+                 </Row>
             </div>
         );
     }
