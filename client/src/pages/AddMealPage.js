8549e7e9dabffd84a1bcecf4e912184917d647c0
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-13 01:14:57 -0500

    added AddMealPage

--- client/src/pages/AddMealPage.js
+++ client/src/pages/AddMealPage.js
@@ -7,21 +7,25 @@ class AddMealPage extends Component {
         return (
             <div>
                 <Row >
-                    <div className='col s6 offset-s1'> 
+                    <div className='col s6 '> 
                         <form >
                             <h5>Add Food</h5>
-                            <Input placeholder="Placeholder" s={12} label="First Name" />
+                            <Input placeholder="Search" s={12} label="Add Food" />
+                            <Input name='group1' type='radio' value='breakfast' label='Breakfast' />
+                            <Input name='group1' type='radio' value='lunch' label='Lunch' />
+                            <Input name='group1' type='radio' value='dinner' label='Dinner'  />
+                            <Input name='group1' type='radio' value='snack' label='Snack'  />
                                 <div class="col s4">
-                                    <p class="center-align"><button class="btn btn-large waves-effect waves-light" type="button" name="action">Search</button></p>
+                                    <p class="left-align"><button class="btn btn-small waves-effect waves-light" type="button" name="action">Search</button></p>
                                 </div>
                         </form>
                     </div>
-                    <div className='col s5 '>
+                    <div className='col s4 offset-s2'>
                         <form>
                             <h5>Add Water</h5>
-                            <Input placeholder="Placeholder" s={12} label="First Name" />
+                            <Input placeholder="Water" s={12} label="Add Water" />
                                 <div class="col s4">
-                                    <p class="center-align"><button class="btn btn-large waves-effect waves-light" type="button" name="action">Add</button></p>
+                                    <p class="right-align"><button class="btn btn-small waves-effect waves-light" type="button" name="action">Add</button></p>
                                 </div>
                         </form>
                     </div>
