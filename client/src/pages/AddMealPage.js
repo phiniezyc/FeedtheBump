7d1f6231e80e2e5cdf0638cb99bbe6da33e54491
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-16 03:11:46 -0500

    cleaned up code to fix some errors: removed div from switch.  Added className where forgot and left class. However, we have input error on settings page and showing a syntax error on bundle.js

--- client/src/pages/AddMealPage.js
+++ client/src/pages/AddMealPage.js
@@ -34,17 +34,15 @@ class AddMealPage extends Component {
                     <div className='col s6 '> 
                         <form onSubmit={this.handleSubmit}>
                             <h5>Add Food</h5>
-                            {/* Putting these in to see results on page Delete after*/}
-                            <h5>Search: {this.state.search}</h5>
-                            <h5>Meal: {this.state.meal}</h5>
+                            
                             <Input onChange={event => this.setState({search: event.target.value})}placeholder="Search" s={12} label="Add Food" />
 
                             <Input onChange={event => this.setState({meal: event.target.value})} name='group1' type='radio' value='breakfast' label='Breakfast' />
                             <Input onChange={event => this.setState({meal: event.target.value})} name='group1' type='radio' value='lunch' label='Lunch' />
                             <Input onChange={event => this.setState({meal: event.target.value})} name='group1' type='radio' value='dinner' label='Dinner'  />
                             <Input onChange={event => this.setState({meal: event.target.value})} name='group1' type='radio' value='snack' label='Snack'  />
-                            <div class="col s4">
-                                <p class="left-align"><button class="btn btn-small waves-effect waves-light" type="submit" name="action">Search</button></p>
+                            <div className="col s4">
+                                <button className="btn btn-small waves-effect waves-light" type="submit" name="action">Search</button>
                             </div>
                         </form>
                     </div>
@@ -52,8 +50,8 @@ class AddMealPage extends Component {
                         <form>
                             <h5>Add Water</h5>
                             <Input placeholder="Water" s={12} label="Add Water" />
-                                <div class="col s4">
-                                <p class="right-align"><button class="btn btn-small waves-effect waves-light" type="button" name="submit">Add</button></p>
+                                <div className="col s4">
+                                <p className="right-align"><button className="btn btn-small waves-effect waves-light" type="button" name="submit">Add</button></p>
                                 </div>
                         </form>
                     </div>
