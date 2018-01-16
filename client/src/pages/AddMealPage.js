32703b05c5f9574fcf0d729e2268b93a4902bfcf
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-16 01:39:08 -0500

    inputs now console.log to the page correctly after removing prevent.default from page

--- client/src/pages/AddMealPage.js
+++ client/src/pages/AddMealPage.js
@@ -3,21 +3,49 @@ import { Row, Input } from 'react-materialize';
 
 
 class AddMealPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            search: "search here for nutritional info",
+            meal: "Input the meal type"
+
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit() {
+       
+        const data = this.state;
+
+        console.log(data);
+        
+        // fetch('/api/form-submit-url', {
+        //     method: 'POST',
+        //     body: data,
+        //     });
+    }
+
     render() {
         return (
             <div>
                 <Row >
                     <div className='col s6 '> 
-                        <form >
+                        <form onSubmit={this.handleSubmit}>
                             <h5>Add Food</h5>
-                            <Input placeholder="Search" s={12} label="Add Food" />
-                            <Input name='group1' type='radio' value='breakfast' label='Breakfast' />
-                            <Input name='group1' type='radio' value='lunch' label='Lunch' />
-                            <Input name='group1' type='radio' value='dinner' label='Dinner'  />
-                            <Input name='group1' type='radio' value='snack' label='Snack'  />
-                                <div class="col s4">
-                                    <p class="left-align"><button class="btn btn-small waves-effect waves-light" type="button" name="action">Search</button></p>
-                                </div>
+                            {/* Putting these in to see results on page Delete after*/}
+                            <h5>Search: {this.state.search}</h5>
+                            <h5>Meal: {this.state.meal}</h5>
+                            <Input onChange={event => this.setState({search: event.target.value})}placeholder="Search" s={12} label="Add Food" />
+
+                            <Input onChange={event => this.setState({meal: event.target.value})} name='group1' type='radio' value='breakfast' label='Breakfast' />
+                            <Input onChange={event => this.setState({meal: event.target.value})} name='group1' type='radio' value='lunch' label='Lunch' />
+                            <Input onChange={event => this.setState({meal: event.target.value})} name='group1' type='radio' value='dinner' label='Dinner'  />
+                            <Input onChange={event => this.setState({meal: event.target.value})} name='group1' type='radio' value='snack' label='Snack'  />
+                            <div class="col s4">
+                                <p class="left-align"><button class="btn btn-small waves-effect waves-light" type="submit" name="action">Search</button></p>
+                            </div>
                         </form>
                     </div>
                     <div className='col s4 offset-s2'>
