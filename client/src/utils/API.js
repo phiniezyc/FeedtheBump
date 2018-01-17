d2bc0961db8f22743a179ee807da5bb00827e9c4
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-17 01:06:17 -0500

    added api routes. It appears to be working but postman is saying can't validate input data

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,23 +1,21 @@
-// These are the mongoose function used w/ api:
+// These are the mongoose function used w/ api: Books Class Example: import
+// axios from "axios";
 
-//Books Class Example: 
-// import axios from "axios";
-
-// export default {
-//   // Gets all books
-//   getBooks: function() {
-//     return axios.get("/api/books");
-//   },
-//   // Gets the book with the given id
-//   getBook: function(id) {
-//     return axios.get("/api/books/" + id);
-//   },
-//   // Deletes the book with the given id
-//   deleteBook: function(id) {
-//     return axios.delete("/api/books/" + id);
-//   },
-//   // Saves a book to the database
-//   saveBook: function(bookData) {
-//     return axios.post("/api/books", bookData);
-//   }
-// };
+export default {
+    // Gets all books
+    getMeals : function () {
+        return axios.get("/api/meals");
+    },
+    // Gets the book with the given id
+    getMeal : function (id) {
+        return axios.get("/api/meals/" + id);
+    },
+    // Deletes the book with the given id
+    deleteMeal : function (id) {
+        return axios.delete("/api/meals/" + id);
+    },
+    // Saves a book to the database
+    saveMeal : function (mealData) {
+        return axios.post("/api/meals", mealData);
+    }
+};
