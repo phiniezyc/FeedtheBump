f91445d39499e4c637486f45cb0986fbd9ebfb39
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-23 23:12:30 -0500

    sterling fix to page

--- client/src/pages/AddMealPage.js
+++ client/src/pages/AddMealPage.js
@@ -1,129 +1,158 @@
-import React, {Component} from 'react';
-import {Row, Input} from 'react-materialize';
+import React, { Component } from 'react';
+import { Row, Input } from 'react-materialize';
 // import axios from "axios";
-import API from "../utils/API";
+import API from '../utils/API';
 
 class AddMealPage extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            food: "No food entered",
-            calories: "No calories entered",
-            meal: "No meal type entered"
+    this.state = {
+      totalMeals: [],
+      food: 'No food entered',
+      calories: 'No calories entered',
+      meal: 'No meal type entered',
+      water: 'no water submitted',
 
-        };
+    };
 
-        this.handleFoodSubmit = this
-            .handleFoodSubmit
-            .bind(this);
+    this.handleFoodSubmit = this
+      .handleFoodSubmit
+      .bind(this);
 
-        this.state = {
-            water: "no water submitted" 
-        };
+    this.handleWaterSubmit = this.handleWaterSubmit.bind(this);
+  }
 
-        this.handleWaterSubmit = this.handleWaterSubmit.bind(this);
+  componentDidMount() {
+    this.loadMeals();
+  }
 
+  loadMeals() {
+    API.getMeals()
+      .then(res =>
+        this.setState({
+          totalMeals: res.data, food: '', calories: '', meal: '', date: '',
+        }))
+      .catch(err => console.log(err));
+    console.log(this.state.totalMeals);
+  }
+
+  handleFoodSubmit() {
+    // const data = this.state;
+    // console.log(data);
+    if (this.state.food && this.state.calories && this.state.meal) {
+      API.saveMeal({
+        food: this.state.food,
+        calories: this.state.calories,
+        meal: this.state.meal,
+
+      }).then(res => this.loadMeals())
+        .catch(err => console.log(err));
     }
+  }
 
-    handleFoodSubmit() {
-        // const data = this.state;
-        // console.log(data);
-        if (this.state.food && this.state.calories && this.state.meal) {
-            API.saveMeal({
-                food: this.state.food,
-                calories: this.state.calories,
-                meal: this.state.meal
-        
-            })
-            //   .then(res => this.loadBooks()) 
-                .catch(err => console.log(err));
-            }
+  handleWaterSubmit() {
+    const waterData = this.state.water;
+    console.log(waterData);
 
-        // fetch('/api/form-submit-url', {     method: 'POST',     body: data,     });
+    if (this.state.water) {
+      API.saveWater({
+        water: this.state.water,
+
+      })
+      //   .then(res => this.loadBooks())
+        .catch(err => console.log(err));
     }
+  }
 
-    handleWaterSubmit() {
-        const waterData = this.state.water;
-        console.log(waterData);
+  render() {
+    console.log(this.state.totalMeals);
+      
+    return (
+      <div>
+        <Row>
+          <div className="col s6 ">
+            <form onSubmit={this.handleFoodSubmit}>
+              <h5>Add Food</h5>
 
-        if (this.state.water) {
-            API.saveWater({
-                water: this.state.water,
-        
-            })
-            //   .then(res => this.loadBooks()) 
-                .catch(err => console.log(err));
-            }
-    }
+              <Input
+                onChange={event => this.setState({ food: event.target.value })}
+                s={12}
+                label="Add Food"
+                placeholder="Food"
+              />
+              <Input
+                onChange={event => this.setState({ calories: event.target.value })}
+                s={12}
+                label="Add Calories"
+                placeholder="Calories"
+              />
 
-    render() {
-        return (
-            <div>
-                <Row >
-                    <div className='col s6 '>
-                        <form onSubmit={this.handleFoodSubmit}>
-                            <h5>Add Food</h5>
-
-                            <Input
-                                onChange={event => this.setState({food: event.target.value})}
-                                s={12}
-                                label="Add Food"placeholder="Food"/>
-                            <Input
-                                onChange={event => this.setState({calories: event.target.value})}
-                                s={12}
-                                label="Add Calories"placeholder="Calories"/>
-
-                            <Input
-                                onChange={event => this.setState({meal: event.target.value})}
-                                name='group1'
-                                type='radio'
-                                value='breakfast'
-                                label='Breakfast'/>
-                            <Input
-                                onChange={event => this.setState({meal: event.target.value})}
-                                name='group1'
-                                type='radio'
-                                value='lunch'
-                                label='Lunch'/>
-                            <Input
-                                onChange={event => this.setState({meal: event.target.value})}
-                                name='group1'
-                                type='radio'
-                                value='dinner'
-                                label='Dinner'/>
-                            <Input
-                                onChange={event => this.setState({meal: event.target.value})}
-                                name='group1'
-                                type='radio'
-                                value='snack'
-                                label='Snack'/>
-                            <div className="col s4">
-                                <button onClick={this.handleFoodSubmit}
-                                    className="btn btn-small waves-effect waves-light"
-                                    type="submit"
-                                    name="action">ADD</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div className='col s4 offset-s2'>
-                        <form>
-                            <h5>Add Water</h5>
-                            <Input onChange={event => this.setState({water: event.target.value})} placeholder="Water" s={12} label="Add Water"/>
-                            <div className="col s4">
-                                <p className="right-align">
-                                    <button onClick={this.handleWaterSubmit}
-                                        className="btn btn-small waves-effect waves-light"
-                                        type="button"
-                                        name="submit">Add</button>
-                                </p>
-                            </div>
-                        </form>
-                    </div>
-                </Row>
-            </div>
-        );
-    }
+              <Input
+                onChange={event => this.setState({ meal: event.target.value })}
+                name="group1"
+                type="radio"
+                value="breakfast"
+                label="Breakfast"
+              />
+              <Input
+                onChange={event => this.setState({ meal: event.target.value })}
+                name="group1"
+                type="radio"
+                value="lunch"
+                label="Lunch"
+              />
+              <Input
+                onChange={event => this.setState({ meal: event.target.value })}
+                name="group1"
+                type="radio"
+                value="dinner"
+                label="Dinner"
+              />
+              <Input
+                onChange={event => this.setState({ meal: event.target.value })}
+                name="group1"
+                type="radio"
+                value="snack"
+                label="Snack"
+              />
+              <div className="col s4">
+                <button
+                  onClick={this.handleFoodSubmit}
+                  className="btn btn-small waves-effect waves-light"
+                  type="submit"
+                  name="action"
+                >ADD
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="col s4 offset-s2">
+            <form>
+              <h5>Add Water</h5>
+              <Input
+                onChange={event => this.setState({ water: event.target.value })}
+                placeholder="Water"
+                s={12}
+                label="Add Water"
+              />
+              <div className="col s4">
+                <p className="right-align">
+                  <button
+                    onClick={this.handleWaterSubmit}
+                    className="btn btn-small waves-effect waves-light"
+                    type="button"
+                    name="submit"
+                  >Add
+                  </button>
+                </p>
+              </div>
+            </form>
+          </div>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default AddMealPage;
