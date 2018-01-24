04e60410e276500fd330ba75d9049be125d55f6a
Star Lee <zabra15@yahoo.com>
2018-01-24 18:31:37 -0500

    controller code and updated HealthArticles.js page

--- client/src/pages/HealthArticle.js
+++ client/src/pages/HealthArticle.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
-import { Collection, CollectionItem, Row} from 'react-materialize';
+import API from '../utils/API';
+import { Row} from 'react-materialize';
 
 //once the API is working, and we see how the articles are delivered, we'll need to adapt layout.
 class HealthArticle extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            articles: []
+            };
+        }
+
+        componentDidMount() {
+            this.loadArticles();
+        }
+
+            loadArticles() {
+                API.getArticles()
+                    .then(res =>
+                        this.setState({
+                            articles: res.data, Title: '', Image: '', Body: '',
+                        }))
+                    .catch(err => console.log(err));
+                console.log(this.state.articles);
+            }
+
+        // axios.get('https://healthfinder.gov/FreeContent/developer/Search.json?api_key=rlaulqdetivhvqxo&keyword=pregnancy')
+        //     .then(res => {
+        //         console.log(res);
+        //         // console.log(res.data.map(topic) =>
+        //
+        //
+        //         // );
+
+
+        //     })
+        //     .catch(error => {
+        //         console.log('Error fetching and parsing data', error);
+        //     });
+        //
+        // }
+
 
     render() {
+        console.log(this.state.articles);
+
         return (
             <div>
                 <Row>
                     <div className='col s12'>
-                        <Collection header='main-article'>
-                            <CollectionItem s={8} href='#'>Today's Article</CollectionItem>
-                        </Collection>
+                        <h2>Today's Article</h2>
+                        <div id="health-info"></div>
                     </div>
                 </Row>
             </div>
--- controllers/healthArticleController.js
+++ controllers/healthArticleController.js
@@ -1,29 +1,30 @@
-const axios = require("axios");
+const axios = require('axios');
 
 module.exports = {
-    search: function (req, res) {
-        const url = "https://healthfinder.gov/FreeContent/developer/Search.json?";
-        const apiKey = "api_key=rlaulqdetivhvqxo";
-        const keyword = "&keyword=pregnancy";
+    search(req, res) {
+        // const url = "https://healthfinder.gov/FreeContent/developer/Search.json?";
+        // const apiKey = "api_key=rlaulqdetivhvqxo";
+        // const keyword = "&keyword=pregnancy";
 
-        const search = url + apiKey + keyword;
+        const search = "https://healthfinder.gov/FreeContent/developer/Search.json?api_key=rlaulqdetivhvqxo&keyword=pregnancy";
 
 
 
 
         axios
             .get(search)
-            .then(response => {
-                console.log(response.data["result"]["topics"][0]);
+            .then((response) => {
+                // console.log(response.data.data.Result.Topics[0]);
 
                 res.send(response.data);
 
             })
             .catch(err => {
                 console.log(err);
-                res.send({err});
+
+                res.send({ err });
             });
 
-    }
+    },
 
 };
