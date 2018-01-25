5b2bc604fcc35a44d38ecb1a7095978975515a08
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2018-01-24 21:43:17 -0500

    The user page.  Pretty plain but if next week is about style then I can definitely hook it up.

--- client/src/pages/UserNotesPage.js
+++ client/src/pages/UserNotesPage.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { Collection, CollectionItem, Row } from 'react-materialize';
 
 
 class UserNotesPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'Log your mood, symptom, and other relevant. '
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A note was posted: ' + this.state.value);
+        event.preventDefault();
+    }
     render() {
         return (
-            <div>
-                <h1>User Notes Page</h1>
-            </div>
+        <div>
+           <form onSubmit={this.handleSubmit}>
+            <label>
+                <h2>Notes</h2>
+        <textarea placeholder={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+            </form>
+        </div>
         );
     }
 }
