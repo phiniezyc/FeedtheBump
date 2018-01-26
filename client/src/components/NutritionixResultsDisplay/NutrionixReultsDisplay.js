709671eeace9ae61705ea30b01f5aa2f0883c80a
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-26 01:31:40 -0500

    cards are displaying a few nutritional stats. Need to add more. WORKING.

--- client/src/components/NutritionixResultsDisplay/NutrionixReultsDisplay.js
+++ client/src/components/NutritionixResultsDisplay/NutrionixReultsDisplay.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardTitle } from 'react-materialize';
+import "./NutritionixResultsDisplay.css";
 
 class NutritonixResultsDisplay extends Component {
     render() {
@@ -11,9 +12,10 @@ class NutritonixResultsDisplay extends Component {
                 {hits
                     ? hits.map((result, i) => (
                         // <h1 key={i}>{result._score}</h1>
-                        <div key={i} className='nutrinixResultsDiv'>
-                            <Card header={<CardTitle reveal  image='' waves='light'/>}
-                                title={result.fields.item_name}reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}><p><a href="#">This is a link</a></p>
+                        <div key={i} className='nutritionixResultDiv'>
+                            <Card className='cardDisplay' header={<CardTitle reveal  image='' waves='light'/>}
+                                title={result.fields.item_name}reveal={<div><p>{`Calories: ${result.fields.nf_calories}`}</p><p>{`Protein: ${result.fields.nf_protein}`}</p></div>}><p><a href="/">This is a link</a></p>
+                                
                             </Card>
                         </div>
                         
