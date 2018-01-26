5470006f79e67faf310995c68f18f63898691db0
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-26 14:47:41 -0500

    reworked map function

--- controllers/nutritionSearchController.js
+++ controllers/nutritionSearchController.js
@@ -1,11 +1,13 @@
 const apiConfig = require('../client/src/utils/API_Keys');
 const axios = require('axios');
 
+
+
 module.exports = {
   search(req, res) {
     const id = apiConfig.nutritionixId;
     const api_key = apiConfig.nutritionixAPIKey;
-    const searchTerm = `butter`;
+    const searchTerm = `apple`;
     const url = `https://api.nutritionix.com/v1_1/search/${searchTerm}?results=0%3A20&cal_min=0&cal_max=50000&fields=*`
 
     const search = `${url}${id}${api_key}`
@@ -13,7 +15,7 @@ module.exports = {
     axios
       .get(search)
       .then((response) => {
-        console.log(response.data.hits[0].fields.item_name);
+        // console.log(response.data.hits[0].fields.item_name);
 
         res.send(response.data);
       })
