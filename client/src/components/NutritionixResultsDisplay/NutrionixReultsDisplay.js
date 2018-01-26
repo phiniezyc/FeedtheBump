7af988b66a91e8ba6c9a35b8fa322f802b71da9f
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-26 15:09:47 -0500

    added in vitamins we need on the cards. Working

--- client/src/components/NutritionixResultsDisplay/NutrionixReultsDisplay.js
+++ client/src/components/NutritionixResultsDisplay/NutrionixReultsDisplay.js
@@ -13,7 +13,7 @@ class NutritonixResultsDisplay extends Component {
                         // <h1 key={i}>{result._score}</h1>
                         <div key={i} className='nutritionixResultDiv'>
                             <Card className='cardDisplay' header={<CardTitle reveal  image='' waves='light'/>}
-                                title={result.fields.item_name}reveal={<div><p>{`Calories: ${result.fields.nf_calories}`}</p><p>{`Protein: ${result.fields.nf_protein}`}</p></div>}> <p><a href="/">This is a link</a></p>
+                                title={result.fields.item_name}reveal={<div><p>{`Calories: ${result.fields.nf_calories}`}</p><p>{`Protein: ${result.fields.nf_protein}`}</p><p>{`Calcium: ${result.fields.nf_calcium_dv}`}</p><p>{`Iron: ${result.fields.nf_iron_dv}`}</p></div>}> <p><a href="/">This is a link</a></p>
                                 <p>{`Brand Name: ${result.fields.brand_name}`}</p>
                             </Card>
                         </div> 
