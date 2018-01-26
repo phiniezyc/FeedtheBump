e9f498dbceb22890cac105334c4ed2bc5270af7b
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-26 00:53:10 -0500

    card displaying results

--- client/src/components/NutritionixResultsDisplay/NutrionixReultsDisplay.js
+++ client/src/components/NutritionixResultsDisplay/NutrionixReultsDisplay.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import { Card, CardTitle } from 'react-materialize';
 
 class NutritonixResultsDisplay extends Component {
     render() {
-        console.log("Test:",this.props.nutritionixResults)
+        console.log("Test:", this.props.nutritionixResults)
 
         const hits = this.props.nutritionixResults.hits;
         return (
             <div className='nutritionixResultsDiv'>
-                {hits ? hits.map((result, i) => (
-                    <h1 key={i}>{result._score}</h1>
-                )) : ''}
+                {hits
+                    ? hits.map((result, i) => (
+                        // <h1 key={i}>{result._score}</h1>
+                        <div key={i} className='nutrinixResultsDiv'>
+                            <Card header={<CardTitle reveal  image='' waves='light'/>}
+                                title={result.fields.item_name}reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}><p><a href="#">This is a link</a></p>
+                            </Card>
+                        </div>
+                        
+                    ))
+                    : ''}
             </div>
         );
     }
