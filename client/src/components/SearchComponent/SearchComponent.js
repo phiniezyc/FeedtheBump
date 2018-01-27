a911e1da939162b5d3426d223a3b588dd5755d44
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-26 20:37:49 -0500

    added in code that doesn't work so commented out except for form which routes to incorrect path

--- client/src/components/SearchComponent/SearchComponent.js
+++ client/src/components/SearchComponent/SearchComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './SearchComponent.css';
+// import axios from 'axios';
 
 class SearchBar extends Component {
   constructor(props) {
@@ -8,15 +9,31 @@ class SearchBar extends Component {
     this.state = {
       // Can set specific default values also!
       input: 'The text you type will show up here!',
-    };
+    };  
+    
   }
+  componentDidUpdate() {
+      this.passSearchTerm();
+  }
+  passSearchTerm = () => {
+    //   let searchTerm = this.state.input;
+    //   axios.post('/api/nutritionix/searchTerm',(req, res)=>{
+    //       searchTerm;
+    //   })
+    //  .then(console.log(searchTerm));
+
+  }
+
   render() {
     return (
       <div className="searchDiv">
         {/* we can use this.state.input to reference but don't use to set!use setState */}
         <h5> {this.state.input} </h5>
         <div className='inputDiv'>
-        <input onChange={event => this.setState({ input: event.target.value })} />
+        <form method="POST" action='/api/nutritionix/searchTerm'>
+        <input onChange={event => this.setState({ input: event.target.value })} type='text' id='search' name='search'/>
+        <button onClick={event => console.log("Submit button works")} className="btn btn-small waves-effect waves-light" type="submit" name="submit"> SUBMIT </button>
+        </form>
         </div>
       </div>
     );
@@ -24,4 +41,4 @@ class SearchBar extends Component {
 }
 
 
-export default SearchBar;
+export default { SearchBar,  };
