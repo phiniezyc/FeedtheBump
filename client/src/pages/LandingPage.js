6e048bdce927d0af25e4150c059c3a3d750b7dc7
Star Lee <zabra15@yahoo.com>
2018-01-28 14:38:16 -0500

    landing page styling

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import { Slider, Slide } from 'react-materialize';
 import Auth from '../utils/Auth.js';
+import '../styles/Main.css';
+import '../styles/LandingPage.css';
+
+
 
 
 const auth = new Auth();
@@ -14,11 +19,18 @@ class LandingPage extends Component {
             // Grid sizing needs tweeking
 
             <div className='col s6 offset-s3'>
-                <div>
-                    <h1>Welcome to Feed the Bump</h1>
+                <div className="banner">
+                    <Slider className="fullscreen">
+                        <Slide className="caption center-align" src="https://static.pexels.com/photos/616412/pexels-photo-616412.jpeg">
+                            <div className="center-align">
+                                <h1>Feed the Bump</h1>
+                                <button onClick = {this.sign_user} className="btn btn-large ftb-button" type="button" name="action">Sign In</button>
+                            </div>
+                        </Slide>
+                    </Slider>
 
-                    <button onClick = {this.sign_user} className="btn btn-large waves-effect waves-light" type="button" name="action">Click to sign in</button>
-                </div>
+
+                    </div>
             </div>
 
         );
