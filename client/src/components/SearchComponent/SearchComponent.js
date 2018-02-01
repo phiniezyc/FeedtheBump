9513350d7e5262ee39e9ddec82509448328cd93c
Chance Phiniezy <phiniezyc@gmail.com>
2018-02-01 18:53:07 -0500

    wip

--- client/src/components/SearchComponent/SearchComponent.js
+++ client/src/components/SearchComponent/SearchComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './SearchComponent.css';
 import axios from 'axios';
-import _ from 'lodash';
 
 class SearchBar extends Component {
   constructor(props) {
@@ -14,16 +13,16 @@ class SearchBar extends Component {
     
   }
 
-  passSearchTerm = _.throttle(() => {
+  passSearchTerm = () => {
     axios.get(`/api/nutritionix?searchTerm=${this.state.input}`)
-    .then(function (response) {
-      this.props.updateResults(response.data);
-      console.log(response.data.hits[0].fields.item_name);
+    .then((response) => {
+      this.props.updateResults(JSON.parse(response.data));
+      console.log(response.data);
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.log(error);
     });
-  }, 800);
+  };
 
 
   render() {
@@ -36,7 +35,10 @@ class SearchBar extends Component {
         {/*// call api GET /api/whatever?search=${event.target.value}
             // .then// this.setState({ results: results })*/}
         <input onChange={event => this.setState({ input: event.target.value })} type='text' id='search' name='search'/>
-        <button onClick={this.passSearchTerm} className="btn btn-small waves-effect waves-light" type="submit" name="submit"> SUBMIT </button>
+        <button onClick={(event) => {
+          event.preventDefault();
+          this.passSearchTerm();
+        }} className="btn btn-small waves-effect waves-light"> SUBMIT </button>
         </form>
         </div>
       </div>
