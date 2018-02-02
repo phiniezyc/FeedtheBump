e7fdae83915d53d6729672a0af1a064789dae5ee
Star Lee <zabra15@yahoo.com>
2018-02-02 12:59:02 -0500

    trying to fix broken search

--- client/src/components/NutritionixResultsDisplay/NutrionixReultsDisplay.js
+++ client/src/components/NutritionixResultsDisplay/NutrionixReultsDisplay.js
@@ -9,13 +9,13 @@ class NutritonixResultsDisplay extends Component {
         return (
             <div className='nutritionixResultsDiv'>
                 {(hits || []).map((result, i) => (
-                        <div key={i} className='nutritionixResultDiv'>
-                            <Card className='cardDisplay' header={<CardTitle reveal  image='' waves='light'/>}
-                                title={result.fields.item_name}reveal={<div><p>{`Calories: ${result.fields.nf_calories}`}</p><p>{`Protein: ${result.fields.nf_protein}`}</p><p>{`Calcium: ${result.fields.nf_calcium_dv}`}</p><p>{`Iron: ${result.fields.nf_iron_dv}`}</p></div>}> 
-                                <p>{`Brand Name: ${result.fields.brand_name}`}</p>
-                            </Card>
-                        </div> 
-                    ))}
+                    <div key={i} className='nutritionixResultDiv'>
+                        <Card className='cardDisplay' header={<CardTitle reveal  image='' waves='light'/>}
+                              title={result.fields.item_name}reveal={<div><p>{`Calories: ${result.fields.nf_calories}`}</p><p>{`Protein: ${result.fields.nf_protein}`}</p><p>{`Calcium: ${result.fields.nf_calcium_dv}`}</p><p>{`Iron: ${result.fields.nf_iron_dv}`}</p></div>}>
+                            <p>{`Brand Name: ${result.fields.brand_name}`}</p>
+                        </Card>
+                    </div>
+                ))}
             </div>
         );
     }
